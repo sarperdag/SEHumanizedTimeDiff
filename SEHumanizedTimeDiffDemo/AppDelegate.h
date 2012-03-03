b206726a861056e07bf67b95ea82a4ddf1082761
Sarp Erdag <sarperdag@Sarps-MacBook-Air.local>
2012-03-03 13:31:25 +0200

    Initial Commit

---
+++ SEHumanizedTimeDiffDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SEHumanizedTimeDiffDemo
+//
+//  Created by Sarp Erdag on 3/3/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
